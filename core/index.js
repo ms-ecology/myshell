31ea2c4a3dbfd23e1cd0a830d71534a6e48152db
anotherso1a <1181581742@qq.com>
2020-06-09 12:40:15 +0800

    feat(bin): complete commands

--- core/index.js
+++ core/index.js
@@ -1,19 +1,14 @@
-const { Command } = require('commander')
-const version = require('../package.json').version
-const { addOption } = require('../util/index')
-const config = require('../config')
+const { Command } = require("commander");
+const version = require("../package.json").version;
+const { addOption } = require("../util/index");
+const config = require("../config");
 
-
-
-const program = new Command()
-program.version(version)
+const program = new Command();
+program.version(version);
 
 // add customer options
-addOption(program, config)
+// addOption(program, config)
 
-program
-  .option('-d, --debug', 'output extra debugging')
-  .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza');
+program.usage("<command> [options]").command("commit", "create a commit");
 
 program.parse(process.argv);
