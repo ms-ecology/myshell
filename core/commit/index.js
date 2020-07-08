b75fcbae16b920dc53df7c35c3c5e3d66664d8a8
anotherso1a <1181581742@qq.com>
2020-07-08 13:30:11 +0800

    feat(commit): change msc command's logic, use stash before pull

--- core/commit/index.js
+++ core/commit/index.js
@@ -7,12 +7,12 @@ const program = new Command();
 
 program
   .option("-p, --push", "push after commit")
-  .option("-P, --nopull", "do not pull before add");
+  .option("-s, --stash", "stash changes and automatic pull origin before commit")
 
 program.parse(process.argv);
 
 const needPush = !!program.push;
-const noPull = !!program.nopull ? "" : "git pull &&";
+const needStash = !!program.stash;
 
 let args = process.argv.slice(2).filter((s) => !s.startsWith("-"));
 
@@ -39,16 +39,16 @@ async function run() {
         return;
       });
   } else {
-    commandStr = `${noPull} git add ${commitFile} && git commit -m "${commitMsg}"`;
+    commandStr = `git add ${commitFile} && git commit -m "${commitMsg}"`;
   }
   if (commitOpt) {
     let template = require("./assets/commit-template");
     var compiled = _.template(template);
     let commitFileText = compiled(commitOpt).trim();
-    commandStr = `${noPull} git add ${commitFile} && git commit -m "${commitFileText}"`;
-    needPush && (commandStr += `&& git push`);
+    commandStr = `git add ${commitFile} && git commit -m "${commitFileText}"`;
   }
-  noPull && console.log("automatic pull...");
+  needStash && (commandStr = `git stash && git pull && git stash pop && ${commandStr}`)
+  needPush && (commandStr += `&& git push`);
   exec(commandStr); //执行语句
 }
 
