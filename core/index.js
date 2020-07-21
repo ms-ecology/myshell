14743a6c2980cb5bf84a11536d6bb9c6d01bc3bf
anotherso1a <1181581742@qq.com>
2020-07-21 15:59:09 +0800

    feat: add npm-publish ci
    supported windows, add automatic npm publish, up version to v0.1.6

--- core/index.js
+++ core/index.js
@@ -8,33 +8,16 @@ const config = getConfig();
 
 const program = new Command();
 program.version(version);
-// const ORIGINCONFIG = {
-//   'commit [commit-message] [file]': {
-//     desc: "create a commit",
-//     path: path.resolve(__dirname, 'commit/index.js'),
-//   },
-//   'template': {
-//     desc: "load your own script or script file",
-//     path: path.resolve(__dirname, 'template/index.js'),
-//   },
-//   'edit <command>': {
-//     desc: "edit a command's source code",
-//     path: path.resolve(__dirname, 'edit/index.js'),
-//   },
-//   'cat <command>': {
-//     desc: "display a command's source code",
-//     path: path.resolve(__dirname, 'cat/index.js'),
-//   }
-// }
+
 // add customer commands
 addCommand(program, config);
 
 program
   .usage("<command> [options]")
   .command("commit [commit-message] [file]", "create a commit", { executableFile: path.resolve(__dirname, 'commit/index.js') })
-  .command("template", "load your own script or script file")
-  .command("edit <command>", "edit a command's source code")
-  .command("cat <command>", "display a command's source code");
+  .command("template", "load your own script or script file", { executableFile: path.resolve(__dirname, 'template/index.js') })
+  .command("edit <command>", "edit a command's source code", { executableFile: path.resolve(__dirname, 'edit/index.js') })
+  .command("cat <command>", "display a command's source code", { executableFile: path.resolve(__dirname, 'cat/index.js') });
 program
   .command("sos")
   .description("if you need some help, call this...")
