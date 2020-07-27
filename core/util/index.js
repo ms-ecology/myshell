ff23fc5b164544e0eb3b33bfdb957ba42179e3b5
anotherso1a <1181581742@qq.com>
2020-07-27 11:35:54 +0800

    feat: make node scripts use commander easier

--- core/util/index.js
+++ core/util/index.js
@@ -9,21 +9,26 @@ const execEnvMap = {
 const addCommand = (program, config = {}) => {
   Object.keys(config).forEach((cmd) => {
     let option = config[cmd];
-    program
-      .command(cmd)
-      .description(option.desc)
-      .action(function (...args) {
-        let commandOptions = args[1] || [];
-        try {
-          Array.isArray(commandOptions)
-            ? (commandOptions = commandOptions.join(" "))
-            : (commandOptions = commandOptions.args.join(" "));
-        } catch {
-          commandOptions = "";
-        }
-        let execEnv = execEnvMap[option.type] || "node";
-        return exec(`${execEnv} ${option.path} ${commandOptions}`);
-      });
+    let execEnv = execEnvMap[option.type] || "node";
+    if (execEnv === "node") {
+      program.command(cmd, option.desc, { executableFile: option.path });
+    } else {
+      program
+        .command(cmd)
+        .description(option.desc)
+        .action(function (...args) {
+          let commandOptions = args[1] || [];
+          try {
+            Array.isArray(commandOptions)
+              ? (commandOptions = commandOptions.join(" "))
+              : (commandOptions = commandOptions.args.join(" "));
+          } catch {
+            commandOptions = "";
+          }
+
+          return exec(`${execEnv} ${option.path} ${commandOptions}`);
+        });
+    }
   });
 };
 
