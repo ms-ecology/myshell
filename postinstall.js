d176b441de2601970b1e8ee4d32fbac0e9627395
anotherso1a <1181581742@qq.com>
2020-11-30 20:25:02 +0800

    chore: release v0.2.0

---
+++ postinstall.js
@@ -0,0 +1,9 @@
+var chalk = require('chalk')
+
+console.log('')
+console.log(chalk.rgb(236,142,59)('If you think "myshell" is a grate tool'))
+console.log(chalk.rgb(236,142,59)('Maybe you can light a star at https://github.com/anotherso1a/myshell'))
+console.log('')
+console.log(chalk.rgb(236,142,59)('And if you have some problem with it'))
+console.log(chalk.rgb(236,142,59)('Make a issue for me, or you can create a pull request'))
+console.log('')
